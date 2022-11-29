8bd934bf3c01336a0b226f984e396a973888ac84
Najeeb Arif <NajeebArif786@outlook.com>
2022-11-29 22:15:38 +0530

    Add Webflux, Blockhound deps

--- src/main/java/narif/poc/netclick/NetclickApplication.java
+++ src/main/java/narif/poc/netclick/NetclickApplication.java
@@ -2,12 +2,17 @@ package narif.poc.netclick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class NetclickApplication {
 
 	public static void main(String[] args) {
+
+		BlockHound.builder()
+				.install();
 		SpringApplication.run(NetclickApplication.class, args);
+
 	}
 
 }
