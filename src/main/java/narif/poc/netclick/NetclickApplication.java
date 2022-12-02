3c9e32f33ab6e7bec1017f9670cf1347deb44a81
Najeeb Arif <NajeebArif786@outlook.com>
2022-12-02 21:55:47 +0530

    Refactor

--- src/main/java/narif/poc/netclick/NetclickApplication.java
+++ src/main/java/narif/poc/netclick/NetclickApplication.java
@@ -1,14 +1,18 @@
 package narif.poc.netclick;
 
+import narif.poc.netclick.config.NonReactiveTypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableR2dbcRepositories
+@EnableR2dbcRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+		classes = NonReactiveTypes.class))
 public class NetclickApplication {
 
 	public static void main(String[] args) {
--- src/main/java/narif/poc/netclick/config/R2dbcConfig.java
+++ src/main/java/narif/poc/netclick/config/R2dbcConfig.java
@@ -5,15 +5,10 @@ import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.core.DatabaseClient;
 
 @Configuration
-@EnableR2dbcRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
-        classes = NonReactiveTypes.class))
 public class R2dbcConfig {
 
     @Bean
